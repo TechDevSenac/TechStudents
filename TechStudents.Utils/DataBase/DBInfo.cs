ca857c1da36cdc906ab6ee8d1e4bc4797d6f129c
marco.angelo <marco.angelo.blu@gmail.com>
2022-12-01 21:56:31 -0300

    ffff

--- TechStudents.Utils/DataBase/DBInfo.cs
+++ TechStudents.Utils/DataBase/DBInfo.cs
@@ -33,7 +33,25 @@ namespace TechStudents.Utils.DataBase
 
         public static void CreateUsersTable()
         {
-            var create = @"CREATE TABLE [USERS](
+            var exist = false;
+
+            using (var conn = new SqlConnection(DBConnection))
+            {
+                var cmd = new SqlCommand("SELECT * FROM ALUNOS", conn);
+                conn.Open();
+
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"{reader[1]}");
+                    }
+                }
+            }
+
+            if (!exist)
+            {
+                var create = @"CREATE TABLE [USERS](
 	                        [ID] [int] NOT NULL,
 	                        [SENHA] [varchar](150) NOT NULL,
 	                        [BLOCKED] [char](1) NULL,
@@ -48,27 +66,28 @@ namespace TechStudents.Utils.DataBase
                                 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
                             ) ON [PRIMARY]
                             GO";
-            var alter1 = @"ALTER TABLE [USERS]  WITH CHECK ADD  CONSTRAINT [FK_USERS_PESSOAS] FOREIGN KEY([PESSOA])
+                var alter1 = @"ALTER TABLE [USERS]  WITH CHECK ADD  CONSTRAINT [FK_USERS_PESSOAS] FOREIGN KEY([PESSOA])
                             REFERENCES [PESSOAS] ([ID])
                             GO";
-            var alter2 = @"ALTER TABLE [USERS] CHECK CONSTRAINT [FK_USERS_PESSOAS]
+                var alter2 = @"ALTER TABLE [USERS] CHECK CONSTRAINT [FK_USERS_PESSOAS]
                             GO";
-            var sequence = @"CREATE SEQUENCE USERS_SEQ START WITH 1 INCREMENT BY 1";
+                var sequence = @"CREATE SEQUENCE USERS_SEQ START WITH 1 INCREMENT BY 1";
 
-            using (var conn = new SqlConnection(DBConnection))
-            {
-                conn.Open();
-                var cmd = new SqlCommand(create, conn);
-                cmd.ExecuteNonQuery();
+                using (var conn = new SqlConnection(DBConnection))
+                {
+                    conn.Open();
+                    var cmd = new SqlCommand(create, conn);
+                    cmd.ExecuteNonQuery();
 
-                cmd = new SqlCommand(alter1, conn);
-                cmd.ExecuteNonQuery();
+                    cmd = new SqlCommand(alter1, conn);
+                    cmd.ExecuteNonQuery();
 
-                cmd = new SqlCommand(alter2, conn);
-                cmd.ExecuteNonQuery();
+                    cmd = new SqlCommand(alter2, conn);
+                    cmd.ExecuteNonQuery();
 
-                cmd = new SqlCommand(sequence, conn);
-                cmd.ExecuteNonQuery();
+                    cmd = new SqlCommand(sequence, conn);
+                    cmd.ExecuteNonQuery();
+                }
             }
         }
     }
