0f88bb74762888bbd3b0d3de4d2050c28781f207
marco.angelo <marco.angelo.blu@gmail.com>
2022-12-01 21:56:17 -0300

    Alteraações Login

--- TechStudents.Web/Controllers/HomeController.cs
+++ TechStudents.Web/Controllers/HomeController.cs
@@ -1,26 +1,53 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using TechStudents.Utils.Entities;
 using TechStudents.Web.Models;
 
 namespace TechStudents.Web.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
+            if (LoginControl._UserLogged == null)
+            {
+                return RedirectToAction("Login");
+            }
+
             return View();
         }
 
         public ActionResult Login()
         {
+            if (LoginControl._UserLogged != null)
+            {
+                return RedirectToAction("Index");
+            }
+
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginModel loginModel)
+        {
+            var user = new User(loginModel.User);
+            var status = user.ValidarSenha(loginModel.Password);
+
+            switch (status)
+            {
+                case LoginStatus.Sucess:
+                    LoginControl._UserLogged = user;
+                    return RedirectToAction("Index");
+                    break;
+                case LoginStatus.InvalidPassword:
+                    loginModel.Message = "Usuário ou senha inválidos!";
+                    break;
+                case LoginStatus.Blocked:
+                    loginModel.Message = "Usuário bloqueado. Entre em contato com o administrador.";
+                    break;
+            }
+
+            return View(loginModel);
         }
 
         public IActionResult Privacy()
